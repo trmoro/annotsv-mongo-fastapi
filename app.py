4ab6fb9f57fa8324815455888f4fa3a5fb124162
trmoro <tristan.moro@outlook.fr>
2024-11-18 10:08:01 +0100

    refont in batch

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 import time
+from bson import ObjectId
 
 #Logger
 from google.cloud import logging
@@ -84,8 +85,8 @@ def single():
 def batch():
 	t = time.time()
 	logger.log_text("AnnotSV Batch")
-	batch_id = request.args.get("batch-id")
-	batch_data = db["cnvhub_batch"].find_one({'batchId':batch_id})["genomicCoordinates"]
+	batch_id = request.args.get("id")
+	batch_data = db["cnvhub_batch"].find_one({'_id':ObjectId(batch_id)})["genomicCoordinates"]
 	compute_annotsv(batch_id, batch_data)
 	logger.log_text(str(round(time.time() - t,2)) + " AnnotSV CNV-Hub finished !")
 	return {"text":"AnnotSV Batch OK !"}
